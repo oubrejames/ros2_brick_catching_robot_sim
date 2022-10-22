7d1ef568429f3f1b7af467a93466628138baf62d
James Oubre <oubrejames@gmail.com>
2022-10-21 21:55:05 -0500

    Pepped run_turtle.launch.py

--- turtle_brick/launch/run_turtle.launch.py
+++ turtle_brick/launch/run_turtle.launch.py
@@ -9,42 +9,42 @@ from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-# Modified code from ROS2 urdf tutorial source code 
+# Modified code from ROS2 urdf tutorial source code
 # Accessed 10/13/2022
 # https://docs.ros.org/en/humble/Tutorials/Intermediate/URDF/Building-a-Visual-Robot-Model-with-URDF-from-Scratch.html
 # https://index.ros.org/p/urdf_tutorial/
 
+
 def generate_launch_description():
-    #Uses show_turtle.launch.py as appropriate to draw the turtle robot in rviz
-        # Start turtle sim node
-        
+    # Uses show_turtle.launch.py as appropriate to draw the turtle robot in rviz
+    # Start turtle sim node
+
     # config = os.path.join(
     #     get_package_share_directory('turtle_brick'),
     #     'config',
     #     'turtle.yaml'
-    #     )    
-    
+    #     )
+
     turtle_brick_path = get_package_share_path('turtle_brick')
     turtle_param_path = turtle_brick_path / 'turtle.yaml'
-    
+
     turtlesim_node = Node(
-            package='turtlesim',
-            executable='turtlesim_node'
-        )
-    
+        package='turtlesim',
+        executable='turtlesim_node'
+    )
+
     run_turtle_node = IncludeLaunchDescription(
-      PythonLaunchDescriptionSource([os.path.join(
-         get_package_share_directory('turtle_brick')),
-         '/show_turtle.launch.py'])
-      )
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('turtle_brick')),
+            '/show_turtle.launch.py'])
+    )
 
     # Start turtle robot node
     turtle_robot_node = Node(
-        package ='turtle_brick',
+        package='turtle_brick',
         executable='turtle_robot',
         parameters=[turtle_param_path]
     )
-    
 
     return LaunchDescription([
         turtle_robot_node,
